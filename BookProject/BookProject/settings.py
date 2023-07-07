62557657887e8f4e3d20c444a0b2678fea0eb0aa
Femina <feminaansar23@gmail.com>
2023-07-07 16:22:00 +0530

    Added pagination in Book project

--- BookProject/BookProject/settings.py
+++ BookProject/BookProject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,7 +125,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK={
-
-    'DEFAULT_PAGINATION_CLASS':"rest_framework.pagination.LimitOffsetPagination"
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'BookApp.pagination.CustomPagination'
 }
