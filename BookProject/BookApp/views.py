4490b3cae2854673421dced01bce9c6cd28b623c
Femina <feminaansar23@gmail.com>
2023-07-07 16:06:58 +0530

    Added pagination in Book project

--- BookProject/BookApp/views.py
+++ BookProject/BookApp/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
+from .pagination import CustomPagination
 
 class BookCreateList(generics.ListCreateAPIView):
     queryset=Book.objects.all()
     serializer_class = BookSerializer
+    pagination_class = CustomPagination
 
 class BookDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset=Book.objects.all()
